7167c926355f39781b30deac1a3082f73ab300b4
Sebastian Hack <hack@cs.uni-saarland.de>
2016-04-07 09:22:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+DBLPbib
+=======
+
+This is a tiny python script to download bibtex entries from [DBLP](http://dblp.uni-trier.de/) automatically.
+It searches the contents of a list of tex files for citations whose bibtex entry is not in a given database yet, downloads the missing entries, and adds them to the database automatically
+
+Howto
+=====
+
+Suppose you use `paper.bib` for your bibliography and have the contents of your paper in files `intro.tex` and `main.tex`.
+Now you want to cite some papers that are not in your bibliography yet.
+Suppose you want to cite Donald Knuth's famous LR-parsing paper.
+You proceed as follows:
+
+1. In his [DBLP entry](http://dblp.uni-trier.de/pers/hd/k/Knuth:Donald_E=), find the paper's dblp key: `journals/iandc/Knuth65` by hovering over the 'arrow down' download symbol.
+2. Now, just add `\cite{DBLP:journals/iandc/Knuth65}` to your text, for example in `main.tex`.
+3. To update your bibliography, just run
+```
+$ ./dblpbib.py -o paper.bib intro.tex main.tex
+```
+
+Notes
+=====
+
+DBLPbib will never erase or modify existing bibtex entries in the specified bibliography.
+It will only append missing entries that it downloaded from DBLP.
---
+++ dblpbib.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+import sys 
+import re
+import argparse
+import urllib.request
+
+dblp_key_pat = '(\w+/)*\w+'
+
+def extract_bibtex_dblp_key(f):
+    pat = re.compile('@\w+{DBLP:(' + dblp_key_pat + ')')
+    for l in f:
+        for m in pat.finditer(l):
+            yield m.group(1)
+
+
+def extract_dblp_cite(f):
+    pat = re.compile(r"\\[a-z]*cite[a-z]*{DBLP:(" + dblp_key_pat + ')}')
+    for l in f:
+        for m in pat.finditer(l):
+            yield m.group(1)
+
+def download_dblp(key):
+    url = 'http://dblp.uni-trier.de/rec/bib2/{}.bib'.format(key)
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = data.decode('utf-8')
+    return text
+
+parser = argparse.ArgumentParser(
+        description="download bibtex entries found in .tex files from DBLP",
+        epilog="""This tools scans the .tex files given as arguments for DBLP citekeys (e.g. \cite{DBLP:journals/iandc/Knuth65}).
+               If the output bibliography (specified by -o) does not contain an appropriate bibtex entry, it 
+               downloads that entry from DBLP and appends it to the output bibliography.
+
+               Other than appending the downloaded entry, it will not modify the output bibliography in any way.""")
+parser.add_argument('infiles', metavar="file", type=str, nargs='+', help='input tex files to scan for citations')
+parser.add_argument("-o", "--output",  required=True, help="output bibliography to add missing entries to", type=str)
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+keys_present = set()
+keys_new     = set()
+
+try:
+    bib = open(args.output, 'r')
+    for key in extract_bibtex_dblp_key(bib):
+        if args.verbose:
+            print('{} in bibfile'.format(key))
+        keys_present.add(key)
+except FileNotFoundError:
+    pass
+
+for fn in args.infiles:
+    with open(fn, 'r') as f, open(args.output, 'a') as out:
+        for key in extract_dblp_cite(f):
+            have_to_download = not key in keys_present and not key in keys_new
+            if args.verbose:
+                print('{}: {}'.format(key, 'downloading' if have_to_download else 'available'))
+            if have_to_download:
+                entry = download_dblp(key)
+                out.write(entry)
+                keys_new.add(key)
